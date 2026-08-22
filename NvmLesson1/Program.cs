1ceafba9164fa93eff040f0738e9d56613929f67
Wofi-Nguyen <wolfkham.01@gmail.com>
2026-08-23 00:21:21 +0700

    NvmLesson1

---
+++ NvmLesson1/Program.cs
@@ -0,0 +1,15 @@
+namespace NvmLesson1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello My!");
+
+            app.Run();
+        }
+    }
+}
